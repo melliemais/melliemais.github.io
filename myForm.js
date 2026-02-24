447b72795ef4c54eac73c21f43955fd4770cdce8
Melissa K Maitino <mk10mait@siena.edu>
2026-02-24 14:31:56 +0000

    added submit button

--- myForm.js
+++ myForm.js
@@ -1,11 +1,17 @@
 let a = "Hello";
 console.log("Hello World");
 const resetButton = document.querySelector("#reset");
+const submitButton = document.querySelector("#submit");
 
-function onClick(){
+function Reset(){
     resetButton.textContent = "Reset!";
+    setTimeout(unClick, 1500);
+}
+function Submit(){
+    submitButton.textContent = "Submitted!";
     setTimeout(unClick, 1500);
 }
 function unClick(){
     resetButton.textContent = "Reset";
+    submitButton.textContent = "Submit";
 }
