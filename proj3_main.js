cf20066e250633e7c3c3a2c89f131b5d04405266
Melissa K Maitino <mk10mait@siena.edu>
2026-04-02 14:10:04 +0000

    number formatting

--- proj3_main.js
+++ proj3_main.js
@@ -1,126 +1,163 @@
 
-function buttons(){
+function buttons() {
 
-    let resetButton = document.getElementById("reset-btn");
-    resetButton.addEventListener("click", reset);
+    document.getElementById("reset-btn").addEventListener("click", reset);
 
-    let exportButton = document.getElementById("export-btn");
-    exportButton.addEventListener("click", exportFunction);
+    document.getElementById("export-btn").addEventListener("click", exportFunction);
 
-    let importButton = document.getElementById("import-btn");
-    importButton.addEventListener("click", importFunction);
-    
-    let incrementButton = document.getElementById("increment-btn");
-    incrementButton.addEventListener("click", function(){
+    document.getElementById("import-btn").addEventListener("click", importFunction);
+
+    document.getElementById("increment-btn").addEventListener("click", function () {
         increment("number", 1);
     });
 
-    for (let i of incrementors){
-        document.getElementById(i+"-btn").addEventListener("click", function(){
-            attemptBuy(i);
+    for (let i of ranks) {
+        document.getElementById(i + "Incrementor-btn").addEventListener("click", function () {
+            attemptBuy(i+"Incrementor");
         })
     }
 }
 
-function increment(item, amt){
-    localStorage.setItem(item, parseInt(localStorage.getItem(item)) + amt);
+function increment(item, amt) {
+    localStorage.setItem(item, parseFloat(localStorage.getItem(item)) + amt);
+    if (item == "number") {
+        increment("totalNumber", amt);
+    }
 }
 
-function attemptBuy(item){
-    let cost = parseInt(localStorage.getItem(item+"-cost"));
-    if (parseInt(localStorage.getItem("number")) >= cost){
-        increment(item+"-quantity", 1);
-        localStorage.setItem(item+"-cost", Math.ceil(cost*1.1));
+function attemptBuy(item) {
+    let cost = parseInt(localStorage.getItem(item + "-cost"));
+    if (parseInt(localStorage.getItem("number")) >= cost) {
+        increment(item + "-quantity", 1);
+        localStorage.setItem(item + "-cost", Math.ceil(cost * 1.1));
         pay(cost);
     }
     adjustVelocity();
 }
 
-function exportFunction(){
+function exportFunction() {
     console.log(JSON.stringify(localStorage));
 }
 
-function importFunction(){
+function importFunction() {
     let importFile = prompt("Insert your savefile here...");
-    if (importFile == null || importFile == ""){
+    if (importFile == null || importFile == "") {
         alert("Import cannot be empty.")
-    }else{
-        try{
-        importFile = JSON.parse(importFile);
-        for (let attribute in localStorage){
-            localStorage.setItem(attribute, importFile[attribute]);
+    } else {
+        try {
+            importFile = JSON.parse(importFile);
+            for (let attribute in localStorage) {
+                localStorage.setItem(attribute, importFile[attribute]);
+            }
+            //localStorage = importFile;
         }
-        //localStorage = importFile;
-        }
-        catch{
+        catch {
             alert("Not a valid file.");
         }
     }
 }
 
-function pay(cost){
+function pay(cost) {
     localStorage.setItem("number", parseInt(localStorage.getItem("number")) - cost);
 }
 
-function numberFormat(float){
-    let power = Math.round(Math.log10(float));
-    if (float > Math.pow(10, 6)){
-    return Math.round(float)/Math.pow(10,power) + "e" + power;
-    }
-    else{
-        return Math.floor(float*10)/10;
+function displayNumber(amt) {
+
+    let float = localStorage.getItem(amt);
+    let power = Math.floor(Math.floor(Math.log10(float)) / 3);
+
+    let roundAmt = Math.floor(float * 10) / 10;
+    let roundedFigs = Math.floor(float / Math.pow(1000, power) * 100) / 100;
+
+    if (float >= 1000) {
+        return roundedFigs + amts[power - 1];
+    } else {
+        return roundAmt;
     }
 }
 
-function refresh(){
+function refresh() {
 
-    if (localStorage.getItem("velocity") === null){
+    if (localStorage.getItem("velocity") === null) {
         reset();
     }
 
-    document.getElementById("number").textContent = numberFormat(localStorage.getItem("number"));
-    document.getElementById("velocity").textContent = "+"+localStorage.getItem("velocity")+" / sec";
+    setContent("number", displayNumber("number"));
+    setContent("velocity", "+" + displayNumber("velocity") + " / sec");
 
-    for (let i of incrementors){
-        document.getElementById(i+"-quantity").textContent = "Owned: " + localStorage.getItem(i+"-quantity");
-        document.getElementById(i+"-cost").textContent = "Cost: " + localStorage.getItem(i+"-cost");
-        document.getElementById(i+"-production").textContent = "Produces: +" + localStorage.getItem(i+"-production") + " / sec";
+    for (let i of ranks) {
+        setContent(i + "Incrementor-quantity", "Owned: " + displayNumber(i + "Incrementor-quantity"));
+        setContent(i + "Incrementor-cost", "Cost: " + displayNumber(i + "Incrementor-cost"));
+        setContent(i + "Incrementor-production", "Produces: +" + displayNumber(i + "Incrementor-production") + " / sec");
+    }
+
+    let totalNumber = localStorage.getItem("totalNumber");
+
+    if (totalNumber < 100) {
+        setContent("totalNumber", "You have a feeling that you're missing something.");
+    } else if (totalNumber < 1000) {
+        setContent("totalNumber", "True, your number has increased, but the world around you still feels off.");
+    } else if (totalNumber < 10000) {
+        setContent("totalNumber", "You've started to figure things out. It's not here; it's the remainder of the web.");
+    } else if (totalNumber < 100000) {
+        setContent("totalNumber", "The outside world is a scary place. Are you willing to venture out into it?");
     }
 
 }
 
-function gain(){
-    localStorage.setItem("number", parseFloat(localStorage.getItem("number")) + parseFloat(localStorage.getItem("velocity") / (1000 / INTERVAL)));
-    
+function setContent(id, toText) {
+    document.getElementById(id).textContent = toText;
+}
+
+function gain() {
+    let gain = parseFloat(localStorage.getItem("velocity") / (1000 / INTERVAL));
+    increment("number", gain);
+
     let exportFile = JSON.stringify(localStorage);
     refresh();
 }
 
-function reset(){
+function reset() {
 
     localStorage.clear();
 
     localStorage.setItem("number", 0);
+    localStorage.setItem("totalNumber", 0);
     localStorage.setItem("velocity", 0);
+    localStorage.setItem("click", 1);
 
-    localStorage.setItem("basicIncrementor-quantity", 0);
-    localStorage.setItem("basicIncrementor-cost", 10);
-    localStorage.setItem("basicIncrementor-production", 1);
+    for (let i = 0; i < ranks.length; i++) {
+        localStorage.setItem(ranks[i] + "Incrementor-quantity", 0);
+        localStorage.setItem(ranks[i] + "Incrementor-cost", Math.pow(10, i + 1));
+        localStorage.setItem(ranks[i] + "Incrementor-production", Math.pow(5, i));
+    }
 
-    localStorage.setItem("advIncrementor-quantity", 0);
-    localStorage.setItem("advIncrementor-cost", 100);
-    localStorage.setItem("advIncrementor-production", 5);
+    localStorage.setItem("basicClicker-quantity", 0);
+    localStorage.setItem("basicClicker-cost", 10);
+    localStorage.setItem("basicClicker-production", 1);
 
-    localStorage.setItem("expIncrementor-quantity", 0);
-    localStorage.setItem("expIncrementor-cost", 1000);
-    localStorage.setItem("expIncrementor-production", 25);
+    localStorage.setItem("stdClicker-quantity", 0);
+    localStorage.setItem("stdClicker-cost", 100);
+    localStorage.setItem("stdClicker-production", 5);
+
+    localStorage.setItem("advClicker-quantity", 0);
+    localStorage.setItem("advClicker-cost", 1000);
+    localStorage.setItem("advClicker-production", 25);
+
+    localStorage.setItem("expClicker-quantity", 0);
+    localStorage.setItem("expClicker-cost", 10000);
+    localStorage.setItem("expClicker-production", 125);
+
+    localStorage.setItem("masClicker-quantity", 0);
+    localStorage.setItem("masClicker-cost", 100000);
+    localStorage.setItem("masClicker-production", 625);
 
 }
 
-function adjustVelocity(){
+function adjustVelocity() {
     let velocity = 0;
-    for (let i of incrementors){
-        velocity += parseFloat(localStorage.getItem(i+"-production") * parseInt(localStorage.getItem(i+"-quantity")));
+    for (let i of ranks) {
+        velocity += parseFloat(localStorage.getItem(i + "Incrementor-production") * parseInt(localStorage.getItem(i + "Incrementor-quantity")));
     }
     localStorage.setItem("velocity", velocity);
 }
@@ -128,6 +165,7 @@ function adjustVelocity(){
 let INTERVAL = 100;
 let refreshRate = setInterval(refresh, 0);
 let gainRate = setInterval(gain, INTERVAL);
-let incrementors = ["basicIncrementor", "advIncrementor", "expIncrementor"];
+let ranks = ["basic", "std", "adv", "exp", "mas"];
+let amts = ["K", "M", "B", "T", "Qa", "Qt", "Sx"];
 
 buttons();
